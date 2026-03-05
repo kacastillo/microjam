45935b26c06bb007b803ba43258fa720f0513065
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-03-05 12:09:21 -0800

    Adding more boiler-plate, updated aub_test_game to jas_game

--- src/jas/jas_game.cpp
+++ src/jas/jas_game.cpp
@@ -1,4 +1,4 @@
-#include "aub_test_game.h"
+#include "jas_game.h"
 
 #include "bn_keypad.h"
 #include "bn_display.h"
@@ -22,3 +22,28 @@ MJ_GAME_LIST_ADD_CODE_CREDITS(code_credits)
 MJ_GAME_LIST_ADD_GRAPHICS_CREDITS(graphics_credits)
 MJ_GAME_LIST_ADD_MUSIC_CREDITS(music_credits)
 MJ_GAME_LIST_ADD_SFX_CREDITS(sfx_credits)
+
+namespace jas {
+  /**
+ * Constructor for an instance of a jas_game
+ * 
+ * First item in the initializer list MUST be a call to the superclass, mj::game with the identifier for the microgame.
+ * 
+ * @param completed_games how many microgames the player has completed so far
+ * @param data shared information, such as a rng and number of frames left in the microgame
+ */
+jas_game::aub_test_game([[maybe_unused]] int completed_games, [[maybe_unused]] const mj::game_data& data) :
+    mj::game("aub"),
+    _player(player({20, 0}, 2))
+    {}
+
+/**
+ * The instructions given to the player at the beginning of the microgame.
+ * 
+ * Must be <= 16 characters long
+ */
+bn::string<16> jas_game::title() const {
+    return "Land the space-ship.";
+}
+
+}
