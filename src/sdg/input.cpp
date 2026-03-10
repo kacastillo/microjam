e171a085bfe2b54a74f4054712ace7184772aed2
KJ <kjmiclea@gmail.com>
2026-03-10 17:53:00 +0000

    Added logging. The game wins, but does not win the game immediately!

--- src/sdg/input.cpp
+++ src/sdg/input.cpp
@@ -1,11 +1,12 @@
 #include "sdg/input.h"
 #include <bn_keypad.h>
 #include "bn_sprite_items_sdg_arrow_sheet.h"
+#include <bn_log.h>
 
 namespace sdg {
 
 input::input() {
-    challenge = { 0, 3, 1, 2, 0 }, // will be made random later
+    challenge = { 0, 1, 2, 3, 0 }, // will be made random later
     progress = 0,
     completed = false;
 };
@@ -13,23 +14,30 @@ input::input() {
 // Reads the input from the D-Pad each frame
 void input::update() {
     
-    if(bn::keypad::up_held()) { _input = 0; }
-    else if(bn::keypad::right_held()) { _input = 1; }
-    else if(bn::keypad::down_held()) { _input = 2; }
-    else if(bn::keypad::left_held()) { _input = 3; }
+    if(bn::keypad::up_pressed()) { _input = 0; }
+    else if(bn::keypad::right_pressed()) { _input = 1; }
+    else if(bn::keypad::down_pressed()) { _input = 2; }
+    else if(bn::keypad::left_pressed()) { _input = 3; }
     // No input results in a default value
     else {_input = -1;}
 
-    if (_input != -1) {
-        if(_input == input::challenge[progress]) { progress += 1; }
+    if (_input != -1 && progress < 5) {
+        if(_input == input::challenge[progress]) {
+            progress += 1;
+            BN_LOG("CODE IS CORRECT!");
+        }
         // reset if incorrect
-        else { progress = 0; }
+        else {
+            progress = 0;
+            BN_LOG("CODE INCORRECT!");
+        }
     }
 }
 
 // if code complete, victory is achieved
 bool input::code_is_correct() const {
     return progress == challenge.size();
+    BN_LOG("VICTORY! (code_is_correct tripped)");
 }
 
 }
